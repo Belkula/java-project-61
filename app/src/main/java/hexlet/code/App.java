fa409e90eac8908a10d08424317b33ba2e5b57a8
Belkula <majin.td@mail.ru>
2024-02-13 19:29:15 +0300

    подчистил app.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -8,29 +8,18 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the game number and press Enter.");
         System.out.print("1 - Greet\n"
-                + "2 - Even\n"
-                + "3 - Calc\n"
-                + "4 - GCD\n"
-                + "5 - Progression\n"
-                + "6 - Prime\n"
-                + "0 - Exit\n"
+                + "2 - Even\n" + "3 - Calc\n"
+                + "4 - GCD\n" + "5 - Progression\n"
+                + "6 - Prime\n" + "0 - Exit\n"
                 + "Your choice: ");
         int game = scanner.nextInt();
         scanner.nextLine();
-        switch (game) {
-            case 6:
-            case 5:
-            case 4:
-            case 3:
-            case 2:
-                Game.launcher(scanner, game);
-                break;
-            case 1:
+        if (game != 0) {
+            if (game == 1) {
                 Cli.username(scanner);
-                break;
-            case 0:
-            default:
-                break;
+            } else {
+                Game.launcher(scanner, game);
+            }
         }
         scanner.close();
     }
